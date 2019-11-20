9fa4aaafbb439f6637b18cf173e1d5bd53bb63f1
ankomahene <ankomaheneshadrack@gmail.com>
2019-11-20 19:13:40 +0000

    init

---
+++ src/main/java/io/turntabl/tcmsProjects/TcmsProjectsApplication.java
@@ -0,0 +1,13 @@
+package io.turntabl.tcmsProjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcmsProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TcmsProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/turntabl/tcmsProjects/TcmsProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntabl.tcmsProjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcmsProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
